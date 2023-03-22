678d7154d6cfdda665791ec91c18f169b9450aa6
chb6734 <chb6734@gmail.com>
2023-03-23 02:47:32 +0900

    feat: router 적용 및 navBar 틀 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Newproduct.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Newproduct() {
+  return <div>Newproduct</div>;
+}
