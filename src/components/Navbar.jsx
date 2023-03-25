4570dda65194308d460530d4d20a7b287f037a04
chb6734 <chb6734@gmail.com>
2023-03-26 03:35:26 +0900

    feat: navbar 틀 완성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
-import { BsFillPencilFill } from "react-icons/bs";
+import { BsFillPencilFill, BsFillCartFill } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to="/">
+    <header className="flex justify-between border-b border-gray-300">
+      {/* <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>ChaSinSa</h1>
+      </Link> */}
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>ChaSinSa</h1>
       </Link>
       <nav>
-        <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
+        <Link to="/products">상품</Link>
+        <Link to="/carts">
+          <BsFillCartFill />
+        </Link>
         <Link to="/products/new">
           <BsFillPencilFill />
         </Link>
