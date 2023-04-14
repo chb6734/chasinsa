fa903e17a3d731186d023beef1a53ec936dfa985
chb6734 <chb6734@gmail.com>
2023-04-14 17:37:20 +0900

    feat: productCard 클릭시 navigate설정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+//product를 풀어주면 Product 전달 불가능함. 그것 때문에 한번 더 추가해줌
 export default function ProductCard({
+  product,
   product: { id, image, title, category, price },
 }) {
+  const navigate = useNavigate();
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105">
+    <li
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+      onClick={() => {
+        navigate(`/products/${id}`, { state: { product } });
+      }}
+    >
       <img className="w-full" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex justify-between">
         <h3 className="truncate">{title}</h3>
