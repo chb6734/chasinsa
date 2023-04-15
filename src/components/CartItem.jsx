3152301e1cdb057af0d2b8890c2e4e4cbfe7d6fd
chb6734 <chb6734@gmail.com>
2023-04-16 03:28:04 +0900

    style: 장바구니 스타일 적용 완료

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -20,10 +20,11 @@ export default function CartItem({
   return (
     <li className="flex justify-between my-2 items-center">
       <img className="w-24 md:w-48 rounded-lg ml-4" src={image} alt={title} />
-      <div className="flex-1 flex justify-between ml-4">
+      <div className="flex-1 flex justify-between mx-4">
         <div className="basis-3/5">
           <p className="text-lg">{title}</p>
           <p className="text-xl font-bold text-red-500">{option}</p>
+          <p className="text-lg font-bold">₩{price * quantity}</p>
         </div>
         <div className="text-2xl flex items-ce">
           <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function PriceCard({ text, price }) {
   return (
-    <div>
+    <div className="w-full bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl mx-5">
       <p>{text}</p>
-      <p>₩{price}</p>
+      <p className="font-bold text-brand text-xl md:text-2xl">₩{price}</p>
     </div>
   );
 }
